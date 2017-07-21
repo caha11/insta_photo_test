46c0aa3dddfc1762b29eb8de9bf7c25b6cf6a6b5
Manu Adina-Zada <manu.adina-zada@hotmail.com>
2017-07-21 04:52:53 +0000

    Added user name to sign up page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
     def destroy  
         @post = Post.find(params[:id])
         @post.destroy
-        redirect_to posts_path
+        redirect_to root_path
     end
     
     def post_params
